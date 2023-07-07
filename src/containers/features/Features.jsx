2aed8c0e8b4fc11176b96729330adbfe5a8ae32c
Alexgodwin113 <89025574+Alexgodwin113@users.noreply.github.com>
2023-07-07 14:22:13 +0100

    final commit

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -24,8 +24,10 @@ const featuresData = [
 const Features = () => (
   <div className="gpt3__features section__padding" id="features">
     <div className="gpt3__features-heading">
-      <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
-      <p>Request Early Access to Get Started</p>
+      <h1 className="gradient__text">The Future is Now and <br/> You Just Need to Realize <br/> It. Step into Future Today. <br/> & Make it Happen.
+      <br /><br />Request Early Access to <br /> Get Started and Subscribe <br />to our Newsletter </h1>
+      
+      
     </div>
     <div className="gpt3__features-container">
       {featuresData.map((item, index) => (
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -3,6 +3,7 @@ import possibilityImage from '../../assets/possibility.png';
 import './possibility.css';
 
 const Possibility = () => (
+ 
   <div className="gpt3__possibility section__padding" id="possibility">
     <div className="gpt3__possibility-image">
       <img src={possibilityImage} alt="possibility" />
@@ -10,10 +11,13 @@ const Possibility = () => (
     <div className="gpt3__possibility-content">
       <h4>Request Early Access to Get Started</h4>
       <h1 className="gradient__text">The possibilities are <br /> beyond your imagination</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <p>Yet bed any for travelling assistance indulgence unpleasing. <br/> Not thoughts all exercise blessing.
+         Indulgence way everything <br/> joy alteration boisterous the attachment. Party we years to order <br/> 
+         allow asked of.</p>
       <h4>Request Early Access to Get Started</h4>
     </div>
   </div>
+  
 );
 
 export default Possibility;
